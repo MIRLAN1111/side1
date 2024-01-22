910e6141f8a233c1b9355a52753df1970a8f37ae
shieun <148762253+MIRLAN1111@users.noreply.github.com>
2024-01-22 10:39:26 +0500

    added files to github

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Expenses } from './Components/Expenses/Expenses';
+import { Main } from './Components/main/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Expenses/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Ul/cards/Cards.jsx
@@ -0,0 +1,17 @@
+import "./Cards.css";
+
+export const Cards = ({ data }) => {
+  return (
+    <div className="containerCards">
+      {data.map((el) => {
+        return (
+          <div className="photos">
+            <img src={el.img} alt={el.name} />
+            <h3>{el.name}</h3>
+            <p>{el.nameG}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
